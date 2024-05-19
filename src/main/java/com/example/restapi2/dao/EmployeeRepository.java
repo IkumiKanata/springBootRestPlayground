60e226d780f9e9ad38090c77aac2e89ace071008
KanataIkumi <i.kanata0531@gmail.com>
2024-05-19 20:55:20 +0900

    add spring data rest

--- src/main/java/com/example/restapi2/dao/EmployeeRepository.java
+++ src/main/java/com/example/restapi2/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.restapi2.dao;
 
 import com.example.restapi2.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
---
+++ src/main/java/com/example/restapi2/rest/EmployeeRestController.java
@@ -0,0 +1,65 @@
+//package com.example.restapi2.rest;
+//
+//import com.example.restapi2.dao.EmployeeDAO;
+//import com.example.restapi2.entity.Employee;
+//import com.example.restapi2.service.EmployeeService;
+//import com.example.restapi2.service.EmployeeServiceImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class EmployeeRestController {
+//
+//    //実際にauto-wiredされるのはEmployeeServiceImpl
+//    private final EmployeeService employeeService;
+//
+//    @Autowired
+//    public EmployeeRestController(EmployeeService theEmployeeService) {
+//        employeeService = theEmployeeService;
+//    }
+//
+//    @GetMapping("/employees")
+//    public List<Employee> findAll() {
+//        return employeeService.findAll();
+//    }
+//
+//    @GetMapping("/employees/{employeeId}")
+//    public Employee getEmployee(@PathVariable int employeeId) {
+//        Employee theEmployee = employeeService.findById(employeeId);
+//        if (theEmployee == null) {
+//            throw new RuntimeException("Employee id not found - " + employeeId);
+//        }
+//        return theEmployee;
+//    }
+//
+//    @PostMapping("/employees")
+//    public Employee addEmployee(@RequestBody Employee theEmployee) {
+//        //also just in case they pass an id in JSON ... set id to 0
+//        //this is to force a save of new item ... instead of update
+//        theEmployee.setId(0);
+//        employeeService.save(theEmployee);
+//        return theEmployee;
+//    }
+//
+//    @PutMapping("/employees")
+//    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+//        employeeService.save(theEmployee);
+//        return theEmployee;
+//    }
+//
+//    @DeleteMapping("/employees/{employeeId}")
+//    public String deleteEmployee(@PathVariable int employeeId) {
+//        Employee tempEmployee = employeeService.findById(employeeId);
+//        //throw exception if null
+//        if (tempEmployee == null) {
+//            throw new RuntimeException("Employee id not found - " + employeeId);
+//        }
+//        employeeService.deleteById(employeeId);
+//        return "Deleted employee id - " + employeeId;
+//    }
+//
+//
+//}
